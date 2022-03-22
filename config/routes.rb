8e7d02523586a1cac4df723d17a277cab6930216
lionel chamouilli <lionel.chamouilli@gmail.com>
2022-03-22 14:46:54 +0100

    Second exercice - Task manager - finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Create a new task
+  # get '/tasks/new', to: 'tasks#new'
+  # post '/tasks', to: 'tasks#create', as: :new_task
+
+  # # Display all tasks & Each Task
+  # get '/tasks', to: 'tasks#index'
+  # get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # # Update task
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch '/tasks/:id', to: 'tasks#update'
+
+  # # Delete task
+  # delete '/tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
